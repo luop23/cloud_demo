0b0785ff5eff9d695ed3ed6535740a6118865bc2
luoping <892420824@qq.com>
2020-05-28 17:36:11 +0800

    使用nacos配置中心及搭建nacos集群并使用nginx做负载

--- cloud_consumer_order7001/src/main/java/com/luop/controller/ConsumerController.java
+++ cloud_consumer_order7001/src/main/java/com/luop/controller/ConsumerController.java
@@ -3,6 +3,7 @@ package com.luop.controller;
 import com.luop.entity.CommonResult;
 import com.luop.service.PaymentFeignService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import javax.annotation.Resource;
  * @Description:
  */
 @RestController
+@RefreshScope   //nacos动态配置刷新   需要结合@value一起使用才会生效
 @RequestMapping("/consumer")
 public class ConsumerController {
 
